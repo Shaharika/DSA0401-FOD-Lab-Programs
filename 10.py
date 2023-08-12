3ae08ab680153c31984eeae2d30c5b66efdaf425
Shaharika <113968799+Shaharika@users.noreply.github.com>
2023-08-12 09:46:22 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,3 +1,4 @@
+#bar plot
 import matplotlib.pyplot as plt
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
 sales = [1000, 1300, 1400, 2500, 1800, 1600]
@@ -13,3 +14,19 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
+
+#line plot
+import matplotlib.pyplot as plt
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+sales = [1000, 1300, 1400, 2500, 1800, 1600]
+
+#create a line plot
+plt.figure(figsize=(8, 4))
+plt.plot(months, sales, marker='o', linestyle='-', color='b')
+plt.xlabel('Months')
+plt.ylabel('Sales')
+plt.title('Monthly Sales Data')
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
